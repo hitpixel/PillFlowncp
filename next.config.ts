5206e9a75327eb29a17cd0108204b2e0e0207417
hitpixel <59998304+hitpixel@users.noreply.github.com>
2025-03-10 20:22:13 +1000

    v1.1.5

--- next.config.ts
+++ next.config.ts
@@ -13,28 +13,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "seo-heist.s3.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "dwdwn8b5ye.ufs.sh",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ansubkhan.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
         hostname: "utfs.io",
         port: "",
         pathname: "/**",
-      },
+      }
     ],
   },
   pageExtensions: ["ts", "tsx", "mdx"],
