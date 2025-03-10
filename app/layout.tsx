d6e34b832401f753a43b5d26c7d94b567f08e486
hitpixel <59998304+hitpixel@users.noreply.github.com>
2025-03-10 20:06:47 +1000

    v1.1.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,19 @@
-import Provider from "@/app/provider";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Analytics } from "@vercel/analytics/react";
 import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { ConvexClientProvider } from "./providers";
 import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://nextstarter.xyz/"),
   title: {
-    default: 'Next Starter',
-    template: `%s | Next Starter`
+    default: 'PillFlow',
+    template: `%s | PillFlow`
   },
-  description:
-    "The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters",
-  openGraph: {
-    description:
-      "The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters",
-    images: [
-      "https://dwdwn8b5ye.ufs.sh/f/MD2AM9SEY8GucGJl7b5qyE7FjNDKYduLOG2QHWh3f5RgSi0c",
-    ],
-    url: "https://nextstarter.xyz/",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Nextjs Starter Kit",
-    description:
-      "The Ultimate Nextjs 15 Starter Kit for quickly building your SaaS, giving you time to focus on what really matters",
-    siteId: "",
-    creator: "@rasmickyy",
-    creatorId: "",
-    images: [
-      "https://dwdwn8b5ye.ufs.sh/f/MD2AM9SEY8GucGJl7b5qyE7FjNDKYduLOG2QHWh3f5RgSi0c",
-    ],
-  },
+  description: "Pharmacy Webster Packing Management System",
 };
 
 export default function RootLayout({
@@ -43,10 +22,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider dynamic>
-      <html lang="en" suppressHydrationWarning>
-        <body className={GeistSans.className}>
-          <Provider>
+    <ClerkProvider>
+      <ConvexClientProvider>
+        <html lang="en" suppressHydrationWarning>
+          <body className={GeistSans.className}>
             <ThemeProvider
               attribute="class"
               defaultTheme="dark"
@@ -56,10 +35,10 @@ export default function RootLayout({
               {children}
               <Toaster />
             </ThemeProvider>
-          </Provider>
-          <Analytics />
-        </body>
-      </html>
+            <Analytics />
+          </body>
+        </html>
+      </ConvexClientProvider>
     </ClerkProvider>
   );
 }
